3ebcdb4ef39d490903de4e10b2f13e1f2e15cdc2
Sunbir Ghag <Dev@Sunbirs-MacBook-Pro.local>
2019-11-02 17:14:16 -0700

    update menu-items with Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 
 
 function App() {
   return (
   <div>
-    <Homepage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+    </Switch>
+
   </div>
   );
 }
