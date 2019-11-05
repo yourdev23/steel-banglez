36b608de39b3d79ca0ba72bc0cac1aef7fc13662
Sunbir Ghag <Dev@Sunbirs-MacBook-Pro.local>
2019-11-05 15:36:21 -0800

    adding header into my code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import './App.css';
 
 
 function App() {
   return (
   <div>
+    <Header/>
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
